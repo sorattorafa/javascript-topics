7ddc0836183ed7fca2a5d4ecd3a529e589146fc4
rafael_soratto <rafael.soratto@appmoove.com.br>
2020-09-07 10:25:55 -0300

    resolve eslint

--- promises/promises2.js
+++ promises/promises2.js
@@ -1,32 +1,34 @@
-function dividePelaMetade(numero){
+function dividePelaMetade(numero) {
     if(numero % 2 !== 0)
         return Promise.reject(new Error("Não posso dividir um número ímpar!"));
     else
         return Promise.resolve(numero / 2);
 }
  
-function soAceitaPares(numero){
-    const promise = new Promise( (resolve, reject) => { 
-                        if (numero % 2 === 0) { 
-                            const resultado = 'Viva, é par!';
-                            resolve(resultado);
-                        } 
-                        else { 
-                            reject(new Error("Você passou um número ímpar!"));
-                        } 
-                    });
+function soAceitaPares(numero) {
+    const promise = new Promise((resolve, reject) => { 
+        if (numero % 2 === 0) { 
+            const resultado = 'Viva, é par!';
+            resolve(resultado);
+        } 
+        else { 
+            reject(new Error("Você passou um número ímpar!"));
+        }});
+    
     return promise;
 }
  
 const numero = 2;
 soAceitaPares(numero)
-    .then(result => dividePelaMetade(numero))
+    .then(() => dividePelaMetade(numero))
     .then(result2 => console.log("A metade de " + numero + " é " + result2))
     .catch(error => console.log("Promises rejected: " + error));
  
 const numeros = [2,4,6,8,10];
 const promises = [];
+
 numeros.forEach(entry => promises.push(dividePelaMetade(entry)));
+
 Promise.all(promises)
     .then(results => results.forEach(entry => console.log(entry)))
     .catch(error => console.log(error));
